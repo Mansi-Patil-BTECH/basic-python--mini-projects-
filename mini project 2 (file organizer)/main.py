d527b6d1fc3b718109d95d51cb87d5b2cbcb028e
Mansi-Patil-BTECH <mansirpatil.01@gmail.com>
2026-07-31 22:20:50 +0530

    code mini project 2

--- mini project 2 (file organizer)/main.py
+++ mini project 2 (file organizer)/main.py
@@ -15,3 +15,14 @@ FILE_TYPES = {
 for folder in FILE_TYPES:  # create folders if not exist
     if not os.path.exists(folder):  #os.path.exists(folder) checks if the folder already exists
         os.mkdir(folder) #os.mkdir is used to create a new directory with the specified name
+ 
+for file in os.listdir(FOLDER_PATH):
+    if os.path.isdir(file):
+        continue
+ 
+    ext = os.path.splitext(file)[1].lower()
+ 
+    for folder, extensions in FILE_TYPES.items():
+        if ext in extensions:
+            shutil.move(file, os.path.join(folder, file))
+            break
