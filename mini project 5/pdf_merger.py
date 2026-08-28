5df1905fa4360f35ccc3e556b373de909e4b4e7c
Mansi-Patil-BTECH <mansirpatil.01@gmail.com>
2026-08-28 16:40:09 +0530

    pdf_merger basic code documentation

---
+++ mini project 5/pdf_merger.py
@@ -0,0 +1,10 @@
+from pypdf import PdfWriter
+merger = PdfWriter()
+# import pypdf
+# merger = pypdf.PdfWriter()
+
+pdfs = ["unit1.pdf", "unit2.pdf"]
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("merge.pdf")
