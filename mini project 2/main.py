fecafe60e57a0f9c0fbad4d12929a083f36832b1
Mansi-Patil-BTECH <mansirpatil.01@gmail.com>
2026-09-01 20:54:48 +0530

    changes

--- mini project 2/main.py
+++ mini project 2/main.py
@@ -1,5 +1,6 @@
+# file organizer project
 import os
-import shutil
+import shutil  # shutil is the module perform operations on the data (read, move, etc)
 
 # organizing path
 FOLDER_PATH = os.getcwd()  # current working directory
@@ -7,24 +8,25 @@ FOLDER_PATH = os.getcwd()  # current working directory
 
 # file directory mapping
 FILE_TYPES = {
+    # you can add more types of file as based on your requirements
     "Images": [".jpg"],
     "Documents": [".pdf", ".docx"],
     "Videos": [".mp4"],
 }
 
 for folder in FILE_TYPES:  # create folders if not exist
-    if not os.path.exists(folder):  #os.path.exists(folder) checks if the folder already exists
-        os.mkdir(folder) #os.mkdir is used to create a new directory with the specified name
- 
-for file in os.listdir(FOLDER_PATH):
-    if os.path.isdir(file):
-        continue
- 
-    ext = os.path.splitext(file)[1].lower()
- 
-    for folder, extensions in FILE_TYPES.items():
-        if ext in extensions:
-            shutil.move(file, os.path.join(folder, file))
-            break
-#run
-#output
+    folder_path = os.path.join(FOLDER_PATH , folder) #os.path.join() is used to join one or more path components together
+    if not os.path.exists(folder_path):  # os.path.exists(folder) checks if the folder already exists
+        os.mkdir(folder_path)  # os.mkdir is used to create a new directory with the specified name
+
+# for file in os.listdir(FOLDER_PATH):
+#     if os.path.isdir(file):
+#         continue
+
+#     ext = os.path.splitext(file)[1].lower()
+
+#     for folder, extensions in FILE_TYPES.items():
+#         if ext in extensions:
+#             shutil.move(file, os.path.join(folder, file))
+#             break
+
