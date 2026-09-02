227bd2384506a2de833e5ab20668c85e80b839f2
Mansi-Patil-BTECH <mansirpatil.01@gmail.com>
2026-09-02 22:36:47 +0530

    file organizer

--- mini project 2/main.py
+++ mini project 2/main.py
@@ -3,7 +3,8 @@ import os
 import shutil  # shutil is the module perform operations on the data (read, move, etc)
 
 # organizing path
-FOLDER_PATH = os.getcwd()  # current working directory
+# FOLDER_PATH = os.getcwd() #this is used when the files are having individual folder
+FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))  # current working directory
 
 
 # file directory mapping
@@ -15,18 +16,33 @@ FILE_TYPES = {
 }
 
 for folder in FILE_TYPES:  # create folders if not exist
-    folder_path = os.path.join(FOLDER_PATH , folder) #os.path.join() is used to join one or more path components together
-    if not os.path.exists(folder_path):  # os.path.exists(folder) checks if the folder already exists
-        os.mkdir(folder_path)  # os.mkdir is used to create a new directory with the specified name
+    folder_path = os.path.join(
+        FOLDER_PATH, folder
+    )  # os.path.join() is used to join one or more path components together
+    if not os.path.exists(
+        folder_path
+    ):  # os.path.exists(folder) checks if the folder already exists
+        os.mkdir(
+            folder_path
+        )  # os.mkdir is used to create a new directory with the specified name
 
-# for file in os.listdir(FOLDER_PATH):
-#     if os.path.isdir(file):
-#         continue
+# organiziing file
+for file in os.listdir(FOLDER_PATH):
+    file_path = os.path.join(FOLDER_PATH, file)
 
-#     ext = os.path.splitext(file)[1].lower()
+    # skip folders
+    if os.path.isdir(file_path):
+        continue
 
-#     for folder, extensions in FILE_TYPES.items():
-#         if ext in extensions:
-#             shutil.move(file, os.path.join(folder, file))
-#             break
+    # get file extensions
+    file_ext = os.path.splitext(file)[
+        1
+    ].lower()  # splittext mein file ka naam aur uski extension dono aa jati h
 
+    for folder, extensions in FILE_TYPES.items():
+        if file_ext in extensions:
+            # shutil.move(file, os.path.join(folder, file))
+            shutil.move(file_path, os.path.join(FOLDER_PATH, folder, file))
+            break
+
+print("FILES ARE ORGANIZED SUCESSFULLY✔️")
